676a98f80bedc6f1b836fea0f5652c55726174c3
machao <machao@home.(none)>
2015-06-21 12:27:36 +0800

    [ZoomIn_ZoomOut_picture:add,high]Add ZoomIn_ZoomOut_picture

--- src/com/machao/paint/MainActivity.java
+++ src/com/machao/paint/MainActivity.java
@@ -30,7 +30,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	private WindowManager wm;
 	
-	private ImageView iv_after, iv_pre;
+	private ImageView iv_1, iv_2;
 	private Bitmap afterbitmap;
 	private Canvas canvas;
 	private Paint paint;
@@ -41,59 +41,34 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainlayout);
 		
-		iv_after = (ImageView)findViewById(R.id.iv_after);
-		iv_pre = (ImageView)findViewById(R.id.iv_pre);
+		iv_1 = (ImageView)findViewById(R.id.iv_1);
+		iv_2 = (ImageView)findViewById(R.id.iv_2);
 		
-		BitmapFactory.Options opts = new Options();
-		opts.inSampleSize = 5;
 		
-		Bitmap after = BitmapFactory.decodeResource(getResources(), R.drawable.after, opts);
-		Bitmap pre = BitmapFactory.decodeResource(getResources(), R.drawable.pre, opts);
+		Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		iv_1.setImageBitmap(bitmap1);
 		
-		afterbitmap = Bitmap.createBitmap(pre.getWidth(), pre.getHeight(), pre.getConfig());
+		Bitmap bitmap2 = Bitmap.createBitmap(bitmap1.getWidth() * 2, bitmap1.getHeight(), bitmap1.getConfig());
 		
 		
 		paint = new Paint();
 		paint.setStrokeWidth(5);
 		paint.setColor(Color.BLACK);
 		
-		canvas = new Canvas(afterbitmap);
+		canvas = new Canvas(bitmap2);
 //		canvas.drawColor(Color.WHITE);
-		canvas.drawBitmap(pre, new Matrix(), paint);
+		Matrix m = new Matrix();
+		m.setValues(new float[]{
+			2.0f, 0, 0,
+			0, 1, 0,
+			0, 0, 1,
+		});
+		//m.setScale(2.0f, 1.0f);
+		canvas.drawBitmap(bitmap1, m, paint);
 		
-		iv_after.setImageBitmap(after);
-		iv_pre.setImageBitmap(afterbitmap);
+		iv_2.setImageBitmap(bitmap2);
 
  
-		iv_pre.setOnTouchListener(new View.OnTouchListener(){
-			int startX, startY, newX, newY;
-			
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				
-				switch(event.getAction()){
-				case MotionEvent.ACTION_DOWN:		
-					break;
-				case MotionEvent.ACTION_MOVE:
-					newX = (int)event.getX();
-					newY = (int)event.getY();
-					for(int i = -8; i < 8; i++)
-						for(int j = -8; j < 8; j++)
-							if((newX + i > 0 && newX + i < afterbitmap.getWidth()) 
-									&& (newY + j > 0 && newY + j < afterbitmap.getHeight()))
-								afterbitmap.setPixel(newX + i, newY + j, Color.TRANSPARENT);
-					iv_pre.setImageBitmap(afterbitmap);
-					break;
-				case MotionEvent.ACTION_UP:
-					break;
-				}
-				
-				return true;
-			}
-			
-			
-		});
 		
 
 	}
